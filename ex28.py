5d3eca45aba3ddd3757920340580a8a42cac2e0e
parwez <parwez.diloo.007@gmail.com>
2021-09-27 22:06:22 +0100

    ex28 updates

---
+++ ex28.py
@@ -0,0 +1,85 @@
+# BOOLEAN PRACTICE - ANSWERS AS COMMENTS
+
+True and True
+# TRUE
+
+False and True
+# FALSE
+
+1 == 1 and 2 == 1
+# TRUE and FALSE
+# FALSE
+
+"test" == "test"
+# TRUE
+
+1 == 1 or 2 != 1
+# TRUE or TRUE
+# TRUE
+
+True and 1 == 1
+# TRUE and TRUE
+# TRUE
+
+False and 0 != 0
+# FALSE and FALSE
+# FALSE
+
+True or 1 == 1
+# TRUE or TRUE
+# TRUE
+
+"test" == "testing"
+# FALSE
+
+1 != 0 and 2 == 1
+# TRUE and FALSE
+# FALSE
+
+"test" != "testing"
+# TRUE
+
+"test" == 1
+# FALSE
+
+not (True and False)
+# not(FALSE)
+# TRUE
+
+not (1 == 1 and 0 != 1)
+# not(TRUE and TRUE)
+# not(TRUE)
+# FALSE
+
+not (10 == 1 or 1000 == 1000)
+# not(FALSE or TRUE)
+# not(TRUE)
+# FALSE
+
+not (1 != 10 or 3 == 4)
+# not(TRUE or FALSE)
+# not(TRUE)
+# FALSE
+
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+# not(TRUE and FALSE)
+# not(FALSE)
+# TRUE
+
+1 == 1 and (not ("testing" == 1 or 1 == 0))
+# TRUE and (not(FALSE or FALSE))
+# TRUE and (not(FALSE))
+# TRUE and TRUE
+# TRUE
+
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))
+# FALSE and (not(FALSE or TRUE))
+# FALSE and (not(TRUE))
+# FALSE and FALSE
+# FALSE
+
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+# TRUE and (not(TRUE or FALSE))
+# TRUE and (not(TRUE))
+# TRUE and FALSE
+# FALSE
